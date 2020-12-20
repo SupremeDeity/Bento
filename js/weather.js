b92202b6362588f21c3542d2a856f2e990725bc5
SupremeDeity <sheikhmohsin706@gmail.com>
2020-12-20 19:02:58 +0500

    Changes to match preferences

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '01bc8b07197877669fec22c90f352b58';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 24.8729551;
+    let longitude = 67.1271219;
 
     getWeather(latitude, longitude);
 }
